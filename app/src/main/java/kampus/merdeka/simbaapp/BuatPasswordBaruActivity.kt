75b917c52ed3c2834e395a2748b7ea7457f40c39
Clinton Dody <clintondody@gmail.com>
2022-11-22 12:26:13 +0700

    lupa password

---
+++ app/src/main/java/kampus/merdeka/simbaapp/BuatPasswordBaruActivity.kt
@@ -0,0 +1,11 @@
+package kampus.merdeka.simbaapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BuatPasswordBaruActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_buat_password_baru)
+    }
+}
