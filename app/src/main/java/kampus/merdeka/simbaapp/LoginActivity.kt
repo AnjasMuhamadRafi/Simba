6af61e0a2f2e2d5eaaacedaad4354711a3fdac7f
AnjasMuhamadRafi <anjas5rafi45@gmail.com>
2022-11-22 12:28:51 +0700

    Login

--- app/src/main/java/kampus/merdeka/simbaapp/LoginActivity.kt
+++ app/src/main/java/kampus/merdeka/simbaapp/LoginActivity.kt
@@ -5,27 +5,38 @@ import androidx.appcompat.app.AppCompatActivity
 import android.content.Intent
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 
 class LoginActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var btnIntent: Button
+    private lateinit var textView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
         supportActionBar?.hide()
 
         btnIntent = findViewById(R.id.btn_Login)
+        val btnIntentRegister = findViewById<TextView>(R.id.btn_register)
 
         btnIntent.setOnClickListener (this)
+        btnIntentRegister.setOnClickListener (this)
 
         }
 
     override fun onClick(v: View) {
         when(v.id){
             R.id.btn_Login -> run {
-                val intentBiasa = Intent(this@LoginActivity, HomeActivity::class.java)
+                val intentBiasa = Intent(this, HomeActivity::class.java)
                 startActivity(intentBiasa)
             }
 
+        }
+        when (v.id){
+            R.id.btn_register -> run {
+                val intentRegister = Intent(this, RegisterActivity::class.java)
+                startActivity(intentRegister)
+            }
         }
     }
 
